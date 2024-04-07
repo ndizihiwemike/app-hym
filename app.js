f6ef99b593617898c5e92bdcf8e406369b76aca4
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-04-07 20:21:59 +0300

    logo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const router = require('./routes/userRoute')
+// const router = require('./routes/userRoute')
 
 const app = express();
 
 app.use(express.static(path.join(__dirname,'./')));
 
-app.use('/',router)
+// app.use('/',router)
 
 app.listen(3000,() =>{
     console.log(`working on port 3000`);
